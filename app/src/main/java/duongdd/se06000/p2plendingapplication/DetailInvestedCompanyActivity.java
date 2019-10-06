1263a8f6fd1b4395f3eb703c33808e1a3f43574e
Đinh Đức Dương <33210825+Duongg@users.noreply.github.com>
2019-10-06 22:09:57 +0700

    detailed UI

---
+++ app/src/main/java/duongdd/se06000/p2plendingapplication/DetailInvestedCompanyActivity.java
@@ -0,0 +1,14 @@
+package duongdd.se06000.p2plendingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailInvestedCompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_invested_company);
+    }
+}
