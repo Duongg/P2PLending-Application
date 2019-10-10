2bfade11906dcf3785fb1556ac3a5d2121a78783
Đinh Đức Dương <33210825+Duongg@users.noreply.github.com>
2019-10-11 00:01:33 +0700

    commit ui 2nd

---
+++ app/src/main/java/duongdd/se06000/p2plendingapplication/UpdateInformationUserActivity.java
@@ -0,0 +1,14 @@
+package duongdd.se06000.p2plendingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateInformationUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_information_user);
+    }
+}
